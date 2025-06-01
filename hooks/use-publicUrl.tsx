e8f138e15a1146e77e5d0f691fbe2982fa4cdd47
theharism <chharis9999@gmail.com>
2025-06-02 01:03:35 +0500

    updated hosted url

--- hooks/use-publicUrl.tsx
+++ hooks/use-publicUrl.tsx
@@ -1,5 +1,5 @@
 
 export function usePublicUrl() {
-    const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://treksor.com';
+    const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://app.treksor.com';
     return baseUrl;
 }
